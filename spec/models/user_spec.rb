00cf8e050af15c16ae4823128594c03da13c73ce
koheihotta191 <kobukuroayaka@gmail.com>
2025-03-22 20:11:53 +0900

    ユーザー管理機能の実装2

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,29 +2,19 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   describe 'ユーザー新規登録' do
-    it 'nicknameが空では登録できない' do
-      # nicknameが空では登録できないテストコードを記述します
-    end
-    it 'emailが空では登録できない' do
-      # emailが空では登録できないテストコードを記述します
-    end
-    it 'passwordが空では登録できない' do
-      # passwordが空では登録できないテストコードを記述します
-    end
-    it 'first_nameが空では登録できない' do
-      # first_nameが空では登録できないテストコードを記述します
-    end
-    it 'last_nameが空では登録できない' do
-      # last_nameが空では登録できないテストコードを記述します
-    end
-    it 'first_name_kanaが空では登録できない' do
-      # first_name_kanaが空では登録できないテストコードを記述します
-    end
-    it 'last_name_kanaが空では登録できない' do
-      # last_name_kanaが空では登録できないテストコードを記述します
-    end
-    it 'birth_dateが空では登録できない' do
-      # birth_dateが空では登録できないテストコードを記述します
+    it '全ての項目が正しく入力されている場合、登録できること' do
+      user = User.new(
+        nickname: '山田太郎ものがたり',
+        email: 'name@gmail.com',
+        password: '6SSZert',
+        password_confirmation: '6SSZert',
+        first_name: '山田',
+        last_name: '太郎',
+        first_name_kana: 'ヤマダ',
+        last_name_kana: 'タロウ',
+        birth_date: '2000-01-01'
+      )
+      expect(user).to be_valid
     end
   end
 end
