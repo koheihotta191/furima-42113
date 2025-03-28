a4564fe6de7108bceb99fa881c89f91aa440d60b
koheihotta191 <kobukuroayaka@gmail.com>
2025-03-28 15:41:09 +0900

    商品情報編集機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+  before_action :correct_user, only: [:edit, :update]
+  
 
   def new
     @item = Item.new
@@ -25,10 +27,43 @@ class ItemsController < ApplicationController
   end
 
 
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path(@item.id)
+    else
+      render :edit, status: :unprocessable_entity
+    end
+
+  end
+
 
   private
 
   def item_params
     params.require(:item).permit(:name, :description, :category_id, :condition_id, :shipping_fee_id, :shipping_area_id, :shipping_day_id, :price, :image)
   end
+
+
+  def correct_user
+    @item = Item.find(params[:id])
+    return if current_user.id == @item.user.id
+    redirect_to root_path
+  end
+
+
+
+
+
+
+
+
+
+
+
+
 end
