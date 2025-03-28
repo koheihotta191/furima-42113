c448ec2af968f1f944737f586a47df4eda4f5978
koheihotta191 <kobukuroayaka@gmail.com>
2025-03-28 16:27:30 +0900

    商品情報編集機能修正2回目

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update]
-  before_action :correct_user, only: [:edit, :update]
   before_action :set_item, only: [:edit, :show, :update]
+  before_action :correct_user, only: [:edit, :update]
   
 
   def new
@@ -48,7 +48,6 @@ class ItemsController < ApplicationController
 
 
   def correct_user
-    @item = Item.find(params[:id])
     return if current_user.id == @item.user.id
     redirect_to root_path
   end
